a1e9d19d92b3eb3623d1c1e50b20f3c27d3a2170
samnayaks2015-cyber <samnayaks2015@gmail.com>
2026-02-26 13:39:40 +0530

    Create cart_item.dart

---
+++ lib/models/cart_item.dart
@@ -0,0 +1,15 @@
+class CartItem {
+  final String id;
+  final String name;
+  final double price;
+  int quantity;
+
+  CartItem({
+    required this.id,
+    required this.name,
+    required this.price,
+    this.quantity = 1,
+  });
+
+  double get total => price * quantity;
+}
