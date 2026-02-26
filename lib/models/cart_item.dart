043d2dfe4eef6b7929e4257994463623fa525275
samnayaks2015-cyber <samnayaks2015@gmail.com>
2026-02-26 15:05:36 +0530

    Update cart_item.dart

--- lib/models/cart_item.dart
+++ lib/models/cart_item.dart
@@ -1,15 +1,11 @@
 class CartItem {
-  final String id;
   final String name;
   final double price;
   int quantity;
 
   CartItem({
-    required this.id,
     required this.name,
     required this.price,
     this.quantity = 1,
   });
-
-  double get total => price * quantity;
 }
