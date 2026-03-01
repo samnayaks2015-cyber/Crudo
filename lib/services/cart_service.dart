8457d400980c532e673060365fe326bd0abc59c3
samnayaks2015-cyber <samnayaks2015@gmail.com>
2026-03-01 07:19:35 -0500

    Update cart_service.dart

--- lib/services/cart_service.dart
+++ lib/services/cart_service.dart
@@ -1,19 +1,25 @@
-import 'cart_item.dart';
+import 'package:flutter/material.dart';
 
-class CartService {
-  final List<CartItem> _items = [];
+class CartService extends ChangeNotifier {
+  final List<Map<String, dynamic>> _items = [];
 
-  List<CartItem> get items => _items;
+  List<Map<String, dynamic>> get items => _items;
 
   void addItem({required String name, required double price}) {
-    _items.add(CartItem(name: name, price: price));
+    _items.add({
+      'name': name,
+      'price': price,
+    });
+    notifyListeners();
   }
 
   void removeItem(int index) {
     _items.removeAt(index);
+    notifyListeners();
   }
 
-  double get totalPrice {
-    return _items.fold(0, (sum, item) => sum + item.price);
-  }
+  double get total =>
+      _items.fold(0, (sum, item) => sum + (item['price'] as double));
+
+  int get count => _items.length;
 }
