da7dd33737584e4cdc252230380fd91270dc7d29
samnayaks2015-cyber <samnayaks2015@gmail.com>
2026-03-01 12:51:27 +0530

    Update cart_service.dart

--- lib/services/cart_service.dart
+++ lib/services/cart_service.dart
@@ -1,51 +1,19 @@
-import 'package:flutter/material.dart';
-import '../models/cart_item.dart';
+import 'cart_item.dart';
 
-class CartService extends ChangeNotifier {
+class CartService {
   final List<CartItem> _items = [];
 
   List<CartItem> get items => _items;
 
-  int get count => _items.fold(0, (sum, item) => sum + item.quantity);
-
-  double get total => _items.fold(
-        0,
-        (sum, item) => sum + (item.price * item.quantity),
-      );
-
   void addItem({required String name, required double price}) {
-    final index = _items.indexWhere((e) => e.name == name);
-
-    if (index >= 0) {
-      _items[index].quantity++;
-    } else {
-      _items.add(CartItem(name: name, price: price, quantity: 1));
-    }
-
-    notifyListeners();
-  }
-
-  void increaseQty(int index) {
-    _items[index].quantity++;
-    notifyListeners();
-  }
-
-  void decreaseQty(int index) {
-    if (_items[index].quantity > 1) {
-      _items[index].quantity--;
-    } else {
-      _items.removeAt(index);
-    }
-    notifyListeners();
+    _items.add(CartItem(name: name, price: price));
   }
 
   void removeItem(int index) {
     _items.removeAt(index);
-    notifyListeners();
   }
 
-  void clearCart() {
-    _items.clear();
-    notifyListeners();
+  double get totalPrice {
+    return _items.fold(0, (sum, item) => sum + item.price);
   }
 }
