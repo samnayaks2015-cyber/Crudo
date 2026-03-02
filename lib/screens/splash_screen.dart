03877f80c3a87777124a289bf546305a9b170f4d
samnayaks2015-cyber <samnayaks2015@gmail.com>
2026-03-02 17:25:24 +0530

    Create splash_screen.dart

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import '../main.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => const MainNavigation()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Image.asset(
+          'assets/images/logo.png',
+          height: 120,
+        ),
+      ),
+    );
+  }
+}
