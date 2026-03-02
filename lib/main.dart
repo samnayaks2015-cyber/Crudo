7a9f8762bfc157c4ac57ee4e7fa87ad222b901e2
samnayaks2015-cyber <samnayaks2015@gmail.com>
2026-03-02 17:23:52 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'screens/home_screen.dart';
+import 'screens/cart_screen.dart';
+import 'screens/splash_screen.dart';
+import 'services/cart_service.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,9 +13,52 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      title: 'CRUDO',
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: const SplashScreen(),
+    );
+  }
+}
+
+class MainNavigation extends StatefulWidget {
+  const MainNavigation({super.key});
+
+  @override
+  State<MainNavigation> createState() => _MainNavigationState();
+}
+
+class _MainNavigationState extends State<MainNavigation> {
+  int _selectedIndex = 0;
+  final CartService cart = CartService();
+
+  @override
+  Widget build(BuildContext context) {
+    final screens = [
+      HomeScreen(cart: cart),
+      CartScreen(cart: cart),
+    ];
+
+    return Scaffold(
+      body: screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.green,
+        onTap: (index) {
+          setState(() => _selectedIndex = index);
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: "Cart",
+          ),
+        ],
+      ),
     );
   }
 }
