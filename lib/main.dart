2be085d0bbb552de529bc42d9dea6a59fb5bd707
samnayaks2015-cyber <samnayaks2015@gmail.com>
2026-03-05 02:23:19 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,74 +1,20 @@
 import 'package:flutter/material.dart';
-import 'screens/home_screen.dart';
-import 'screens/cart_screen.dart';
-import 'services/cart_service.dart';
+import 'screens/splash_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const CrudoApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class CrudoApp extends StatelessWidget {
+  const CrudoApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'CRUDO',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      home: const MainNavigation(),
-    );
-  }
-}
-
-class MainNavigation extends StatefulWidget {
-  const MainNavigation({super.key});
-
-  @override
-  State<MainNavigation> createState() => _MainNavigationState();
-}
-
-class _MainNavigationState extends State<MainNavigation> {
-  int _selectedIndex = 0;
-  final CartService cart = CartService();
-
-  void goToCart() {
-    setState(() {
-      _selectedIndex = 1;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final screens = [
-      HomeScreen(
-        cart: cart,
-        onCartTap: goToCart,
-      ),
-      CartScreen(cart: cart),
-    ];
-
-    return Scaffold(
-      body: screens[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.green,
-        onTap: (index) {
-          setState(() => _selectedIndex = index);
-        },
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: "Home",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.shopping_cart),
-            label: "Cart",
-          ),
-        ],
-      ),
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: const SplashScreen(),
     );
   }
 }
