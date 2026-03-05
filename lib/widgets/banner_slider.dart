6fe7687a76c6bfc9e2edbbfd0529bd9a7d30ff76
samnayaks2015-cyber <samnayaks2015@gmail.com>
2026-03-05 09:32:53 +0530

    Update banner_slider.dart

--- lib/widgets/banner_slider.dart
+++ lib/widgets/banner_slider.dart
@@ -9,46 +9,38 @@ class BannerSlider extends StatefulWidget {
 
 class _BannerSliderState extends State<BannerSlider> {
 
-  final PageController controller = PageController();
-
-  int currentPage = 0;
-
   final List<String> banners = [
     "assets/images/banner1.jpg",
     "assets/images/banner2.jpg",
     "assets/images/banner3.jpg",
   ];
 
+  PageController controller = PageController();
+
+  int currentPage = 0;
+
   @override
   Widget build(BuildContext context) {
-
     return Column(
-
       children: [
 
         SizedBox(
-          height: 180,
-
+          height: 160,
           child: PageView.builder(
             controller: controller,
             itemCount: banners.length,
-
-            onPageChanged: (index){
+            onPageChanged: (index) {
               setState(() {
                 currentPage = index;
               });
             },
-
-            itemBuilder: (context,index){
-
-              return Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16),
-
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(16),
-
-                  child: Image.asset(
-                    banners[index],
+            itemBuilder: (context, index) {
+              return Container(
+                margin: const EdgeInsets.symmetric(horizontal: 12),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  image: DecorationImage(
+                    image: AssetImage(banners[index]),
                     fit: BoxFit.cover,
                   ),
                 ),
@@ -56,3 +48,14 @@ class _BannerSliderState extends State<BannerSlider> {
             },
           ),
         ),
+
+        const SizedBox(height: 8),
+
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: List.generate(
+            banners.length,
+            (index) => Container(
+              margin: const EdgeInsets.symmetric(horizontal: 4),
+              width: currentPage == index ? 10 : 6,
+              height: currentPage == index ? 10 : 6,
