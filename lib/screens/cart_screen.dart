fa615947b69c7d325d8555c3470e04bec977e2fb
samnayaks2015-cyber <samnayaks2015@gmail.com>
2026-03-06 18:17:18 +0530

    Update cart_screen.dart

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,31 +1,41 @@
-import 'package:flutter/material.dart';
-import '../services/cart_service.dart';
-
-class CartScreen extends StatelessWidget {
-  const CartScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-
-      appBar: AppBar(
-        title: const Text("My Cart"),
-      ),
-
-      body: CartService.cart.isEmpty
-          ? const Center(
-          child: Text("Cart is empty"))
-          : ListView(
+body: CartService.cart.isEmpty
+    ? const Center(
+        child: Text(
+          "Your cart is empty",
+          style: TextStyle(fontSize: 18),
+        ),
+      )
+    : ListView(
         children: CartService.cart.entries.map((item){
 
-          return ListTile(
-            title: Text(item.key),
-            trailing: Text("Qty: ${item.value}"),
+          int price = item.key == "Cow Milk" ? 90 : 130;
+
+          return Card(
+            margin: const EdgeInsets.symmetric(
+              horizontal: 16,
+              vertical: 8,
+            ),
+
+            child: ListTile(
+
+              title: Text(
+                item.key,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+              subtitle: Text("Quantity: ${item.value}"),
+
+              trailing: Text(
+                "₹${item.value * price}",
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.green,
+                ),
+              ),
+            ),
           );
 
         }).toList(),
       ),
-    );
-  }
-}
