4b004da1e4a8023ac0cfa621a0288b0c0fbfe775
samnayaks2015-cyber <samnayaks2015@gmail.com>
2026-03-06 17:20:12 +0530

    Update banner_slider.dart

--- lib/widgets/banner_slider.dart
+++ lib/widgets/banner_slider.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class BannerSlider extends StatefulWidget {
-  const BannerSlider({Key? key}) : super(key: key);
+  const BannerSlider({super.key});
 
   @override
   State<BannerSlider> createState() => _BannerSliderState();
@@ -9,19 +9,17 @@ class BannerSlider extends StatefulWidget {
 
 class _BannerSliderState extends State<BannerSlider> {
 
-  final PageController controller = PageController();
+  final List<String> banners = [
+    "assets/images/banner1.jpg",
+    "assets/images/banner2.jpg",
+    "assets/images/banner3.jpg",
+  ];
 
   int currentIndex = 0;
-
-  final List<String> banners = [
-    "assets/images/banner1.png",
-    "assets/images/banner2.png",
-    "assets/images/banner3.png",
-  ];
+  final PageController controller = PageController();
 
   @override
   Widget build(BuildContext context) {
-
     return Column(
       children: [
 
@@ -38,12 +36,16 @@ class _BannerSliderState extends State<BannerSlider> {
             itemBuilder: (context, index) {
 
               return Container(
-                margin: const EdgeInsets.symmetric(horizontal: 12),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  image: DecorationImage(
-                    image: AssetImage(banners[index]),
+                margin: const EdgeInsets.symmetric(
+                  horizontal: 12,
+                  vertical: 8,
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(16),
+                  child: Image.asset(
+                    banners[index],
                     fit: BoxFit.cover,
+                    width: double.infinity,
                   ),
                 ),
               );
@@ -52,30 +54,25 @@ class _BannerSliderState extends State<BannerSlider> {
           ),
         ),
 
-        const SizedBox(height: 10),
+        const SizedBox(height: 8),
 
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: List.generate(
             banners.length,
-            (index) {
-
-              return Container(
-                margin: const EdgeInsets.symmetric(horizontal: 4),
-                width: currentIndex == index ? 10 : 6,
-                height: currentIndex == index ? 10 : 6,
-                decoration: BoxDecoration(
-                  color: currentIndex == index
-                      ? Colors.green
-                      : Colors.grey,
-                  shape: BoxShape.circle,
-                ),
-              );
-
-            },
+            (index) => Container(
+              margin: const EdgeInsets.symmetric(horizontal: 4),
+              width: currentIndex == index ? 10 : 6,
+              height: currentIndex == index ? 10 : 6,
+              decoration: BoxDecoration(
+                color: currentIndex == index
+                    ? Colors.green
+                    : Colors.grey,
+                shape: BoxShape.circle,
+              ),
+            ),
           ),
-        ),
-
+        )
       ],
     );
   }
