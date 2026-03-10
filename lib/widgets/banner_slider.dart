c78d61476be351bee2a92a8a050d6e649ed86cd1
samnayaks2015-cyber <samnayaks2015@gmail.com>
2026-03-10 23:04:06 +0530

    Update banner_slider.dart

--- lib/widgets/banner_slider.dart
+++ lib/widgets/banner_slider.dart
@@ -1,81 +1,20 @@
-import 'package:flutter/material.dart';
-
-class BannerSlider extends StatefulWidget {
-  const BannerSlider({super.key});
-
-  @override
-  State<BannerSlider> createState() => _BannerSliderState();
-}
-
-class _BannerSliderState extends State<BannerSlider> {
-
-  final List<String> banners = [
-    "assets/images/banner1.png",
-    "assets/images/banner2.png",
-    "assets/images/banner3.png",
-  ];
-
-  int currentIndex = 0;
-  final PageController controller = PageController();
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Column(
-      children: [
-
-        SizedBox(
-          height: 170,
-          child: PageView.builder(
-            controller: controller,
-            itemCount: banners.length,
-            onPageChanged: (index) {
-              setState(() {
-                currentIndex = index;
-              });
-            },
-            itemBuilder: (context, index) {
-
-              return Container(
-                margin: const EdgeInsets.symmetric(
-                  horizontal: 16,
-                  vertical: 8,
-                ),
-
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(18),
-
-                  child: Image.asset(
-                    banners[index],
-                    fit: BoxFit.cover,
-                    width: double.infinity,
-                  ),
-                ),
-              );
-            },
-          ),
+CarouselSlider(
+  options: CarouselOptions(
+    height: 170,
+    autoPlay: true,
+    viewportFraction: 0.9,
+    enlargeCenterPage: true,
+  ),
+  items: banners.map((item) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 5),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        image: DecorationImage(
+          image: AssetImage(item),
+          fit: BoxFit.cover,
         ),
-
-        const SizedBox(height: 6),
-
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: List.generate(
-            banners.length,
-            (index) => Container(
-              margin: const EdgeInsets.symmetric(horizontal: 4),
-              width: currentIndex == index ? 10 : 6,
-              height: currentIndex == index ? 10 : 6,
-              decoration: BoxDecoration(
-                color: currentIndex == index
-                    ? Colors.green
-                    : Colors.grey,
-                shape: BoxShape.circle,
-              ),
-            ),
-          ),
-        )
-      ],
+      ),
     );
-  }
-}
+  }).toList(),
+)
