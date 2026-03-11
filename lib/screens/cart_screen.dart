6f2e45e20fdf80c89df2fcd5a7b2ff4276cd31ef
samnayaks2015-cyber <samnayaks2015@gmail.com>
2026-03-11 09:25:22 +0530

    Update cart_screen.dart

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -5,104 +5,16 @@ class CartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-
       appBar: AppBar(
         title: const Text("My Cart"),
-        backgroundColor: Colors.green,
       ),
-
-      body: Column(
-        children: [
-
-          cartItem("Cow Milk","₹90"),
-          cartItem("Buffalo Milk","₹120"),
-
-          const Spacer(),
-
-          Container(
-            padding: const EdgeInsets.all(16),
-            decoration: const BoxDecoration(
-              color: Colors.white,
-              boxShadow: [
-                BoxShadow(color: Colors.grey,blurRadius: 5)
-              ],
-            ),
-
-            child: Column(
-              children: [
-
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: const [
-
-                    Text("Total",
-                        style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold
-                        )),
-
-                    Text("₹210",
-                        style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold
-                        )),
-
-                  ],
-                ),
-
-                const SizedBox(height:10),
-
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.green,
-                    minimumSize: const Size(double.infinity,50),
-                  ),
-                  onPressed: () {},
-                  child: const Text("Checkout"),
-                )
-
-              ],
-            ),
-          )
-
-        ],
+      body: const Center(
+        child: Text(
+          "Cart is Empty",
+          style: TextStyle(fontSize: 20),
+        ),
       ),
-    );
-  }
-
-  Widget cartItem(String name,String price){
-
-    return ListTile(
-
-      leading: const CircleAvatar(
-        backgroundColor: Colors.green,
-      ),
-
-      title: Text(name),
-
-      subtitle: Text(price),
-
-      trailing: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-
-          IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.remove_circle)
-          ),
-
-          const Text("1"),
-
-          IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.add_circle)
-          )
-
-        ],
-      ),
-
     );
   }
 }
