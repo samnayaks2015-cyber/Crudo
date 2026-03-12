dbb5c58007c49094109e5874ec8ebe8027400881
samnayaks2015-cyber <samnayaks2015@gmail.com>
2026-03-12 23:51:44 +0530

    Create product.dart

---
+++ lib/data/product.dart
@@ -0,0 +1,65 @@
+import '../models/product.dart';
+
+List<Product> milkProducts = [
+
+  Product(
+    name: "Cow Milk",
+    image: "assets/images/milk.png",
+    price: 60,
+  ),
+
+  Product(
+    name: "Buffalo Milk",
+    image: "assets/images/milk.png",
+    price: 70,
+  ),
+
+];
+
+List<Product> fruitProducts = [
+
+  Product(
+    name: "Apple",
+    image: "assets/images/fruits.png",
+    price: 120,
+  ),
+
+  Product(
+    name: "Banana",
+    image: "assets/images/fruits.png",
+    price: 60,
+  ),
+
+];
+
+List<Product> vegetableProducts = [
+
+  Product(
+    name: "Tomato",
+    image: "assets/images/vegetables.png",
+    price: 40,
+  ),
+
+  Product(
+    name: "Carrot",
+    image: "assets/images/vegetables.png",
+    price: 50,
+  ),
+
+];
+
+List<Product> dairyProducts = [
+
+  Product(
+    name: "Curd",
+    image: "assets/images/dairy.png",
+    price: 50,
+  ),
+
+  Product(
+    name: "Butter",
+    image: "assets/images/dairy.png",
+    price: 90,
+  ),
+
+];
