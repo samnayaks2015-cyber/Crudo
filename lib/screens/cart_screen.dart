a53475ec50338799ec1ce92a8604e22c615ba394
samnayaks2015-cyber <samnayaks2015@gmail.com>
2026-03-12 23:53:08 +0530

    Update cart_screen.dart

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
 
 class CartScreen extends StatelessWidget {
+
   const CartScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
+
       appBar: AppBar(
         title: const Text("Cart"),
       ),
+
       body: const Center(
-        child: Text("Your cart is empty"),
+        child: Text(
+          "Your cart is empty",
+          style: TextStyle(fontSize: 18),
+        ),
       ),
+
     );
   }
 }
