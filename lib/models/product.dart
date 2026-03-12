63641b29d0cc4531b28a7e43a2e1a2c2cf3bb176
samnayaks2015-cyber <samnayaks2015@gmail.com>
2026-03-12 23:59:16 +0530

    Update product.dart

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,13 +1,13 @@
 class Product {
 
   final String name;
-  final String image;
   final int price;
+  final String image;
 
   Product({
     required this.name,
-    required this.image,
     required this.price,
+    required this.image,
   });
 
 }
