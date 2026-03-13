419663919edf75d619cd6267a1f374889d4aa15b
samnayaks2015-cyber <samnayaks2015@gmail.com>
2026-03-13 10:37:37 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,39 @@
-import 'package:flutter/material.dart';
-import 'screens/splash_screen.dart';
+name: crudo_app
+description: CRUDO Milk, Fruits & Vegetables Delivery
+publish_to: 'none'
 
-void main() {
-  runApp(const CrudoApp());
-}
+version: 1.0.0+1
 
-class CrudoApp extends StatelessWidget {
-  const CrudoApp({super.key});
+environment:
+  sdk: ">=3.0.0 <4.0.0"
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "CRUDO",
-      theme: ThemeData(primarySwatch: Colors.green),
-      home: const SplashScreen(),
-    );
-  }
-}
+dependencies:
+  flutter:
+    sdk: flutter
+
+  cupertino_icons: ^1.0.6
+  carousel_slider: ^4.2.1
+
+  # Firebase
+  firebase_core: ^2.24.0
+  firebase_auth: ^4.15.0
+  cloud_firestore: ^4.13.0
+
+  # Location
+  geolocator: ^10.1.0
+  geocoding: ^2.1.1
+
+  # Payment
+  razorpay_flutter: ^1.3.5
+
+dev_dependencies:
+  flutter_test:
+    sdk: flutter
+
+  flutter_lints: ^3.0.0
+
+flutter:
+  uses-material-design: true
+
+  assets:
+    - assets/images/
