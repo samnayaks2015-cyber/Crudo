b0c56404ebc03e3a804bf6f60dde243df7859a3e
samnayaks2015-cyber <samnayaks2015@gmail.com>
2026-03-13 10:26:37 +0530

    Update checkout_screen.dart

--- lib/screens/checkout_screen.dart
+++ lib/screens/checkout_screen.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 
 class CheckoutScreen extends StatelessWidget {
-  const CheckoutScreen({super.key});
+
+  final double total;
+
+  const CheckoutScreen({
+    super.key,
+    required this.total,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -13,10 +19,73 @@ class CheckoutScreen extends StatelessWidget {
         backgroundColor: Colors.green,
       ),
 
-      body: const Center(
-        child: Text(
-          "Checkout system coming soon",
-          style: TextStyle(fontSize:18),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+
+        child: Column(
+
+          crossAxisAlignment: CrossAxisAlignment.start,
+
+          children: [
+
+            const Text(
+              "Delivery Address",
+              style: TextStyle(
+                fontSize:18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            const SizedBox(height:10),
+
+            TextField(
+              decoration: InputDecoration(
+                hintText: "Enter address",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+            ),
+
+            const SizedBox(height:30),
+
+            Text(
+              "Total: ₹$total",
+              style: const TextStyle(
+                fontSize:22,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            const SizedBox(height:30),
+
+            SizedBox(
+              width: double.infinity,
+              height:55,
+
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green,
+                ),
+
+                onPressed: (){
+
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text("Order Placed Successfully"),
+                    ),
+                  );
+
+                },
+
+                child: const Text(
+                  "PLACE ORDER",
+                  style: TextStyle(fontSize:18),
+                ),
+              ),
+            )
+
+          ],
         ),
       ),
 
